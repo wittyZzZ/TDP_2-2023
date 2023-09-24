1d7093062de94cd3a53f557ab2c10253b42bdab7
wittyZzZ <william.zubarzo@usach.cl>
2023-09-24 17:33:56 -0300

    xd

--- Taller de Programación 2-2023/Lab 1 - Rush Hour/main.cpp
+++ Taller de Programación 2-2023/Lab 1 - Rush Hour/main.cpp
@@ -19,51 +19,6 @@ int main() {
     game->printBoard();
     initial->printRhBoard();
 
-/*
-   Stack old(1);
-
-   StateRH* s1 = new StateRH();
-   s1->setHeurValue(10);
-   StateRH* s2 = new StateRH();
-   s2->setHeurValue(15);
-   StateRH* s3 = new StateRH();
-   s3->setHeurValue(25);
-   StateRH* s4 = new StateRH();
-   s4->setHeurValue(4);
-   StateRH* s5 = new StateRH();
-   s5->setHeurValue(87);
-   StateRH* s6 = new StateRH();
-   s6->setHeurValue(44);
-   StateRH* s7 = new StateRH();
-   s7->setHeurValue(21);
-   StateRH* s8 = new StateRH();
-   s8->setHeurValue(19);
-   StateRH* s9 = new StateRH();
-   s9->setHeurValue(57);
-   StateRH* s10 = new StateRH();
-   s10->setHeurValue(37);
-
-   old.push(s1);
-   old.quickSort(old.stack,0,old.size-1);
-   old.push(s2);
-   old.quickSort(old.stack,0,old.size-1);
-   old.push(s3);
-   old.push(s4);
-   old.push(s5);
-   old.push(s6);
-   old.quickSort(old.stack,0,old.size-1);
-   old.push(s7);
-   old.quickSort(old.stack,0,old.size-1);
-   old.push(s8);
-   old.push(s9);
-   old.push(s10);
-
-   old.quickSort(old.stack,0,old.size-1);
-
-   for (int i = 0; i < 10;i++) {
-    cout << old.stack[i]->getHeurValue() << endl;
-   }
-    */
     StateRH* resolution = game->solver(initial);
 
     if (resolution != nullptr) {
